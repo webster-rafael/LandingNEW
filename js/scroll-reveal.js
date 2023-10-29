299aebb9351db4e33f6d73351ab3a68b3eace1bc
Webster Dev <webster.rafael2014@gmail.com>
2023-10-29 20:56:22 -0300

    NEW

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -9,13 +9,4 @@ window.sr = ScrollReveal({ reset: true});
     distance: '200px', // distância do deslocamento
     delay: 200 });
 
-    ScrollReveal().reveal('#about', { duration: 3000, // duração da animação em milissegundos
-    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
-    distance: '200px', // distância do deslocamento
-    delay: 200 });
-
-    ScrollReveal().reveal('.about-section', { duration: 2000, // duração da animação em milissegundos
-    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
-    distance: '200px', // distância do deslocamento
-    delay: 200 });
-
+    
